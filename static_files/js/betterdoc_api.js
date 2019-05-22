9e35080c243d59c005d30954a1cdbf54ee0508a4
Janselle Justo <jcjusto@ucsd.edu>
2019-05-21 21:52:51 -0700

    put better doc using backend

--- static_files/js/betterdoc_api.js
+++ static_files/js/betterdoc_api.js
@@ -1,28 +1,38 @@
-var specialty = "";
+$(document).ready(()=>{
+    // Initialize Firebase
 
-/*Getting Location from User... still needs work
-function setUpLocation() {
-  if (navigator.geolocation) {
-    window.location = navigator.geolocation.getCurrentPosition();
-  } else {
-    console.log = "Geolocation is not supported by this browser";
-  }
-  var input = document.getElementById('search_doctor');
-}*/
+    var firebaseConfig = {
+      apiKey: "AIzaSyD3iiCraOvePJcqCUSQdEITTD0cjG2ArBw",
+      authDomain: "taaj-cogs121-project.firebaseapp.com",
+      databaseURL: "https://taaj-cogs121-project.firebaseio.com",
+      projectId: "taaj-cogs121-project",
+      storageBucket: "taaj-cogs121-project.appspot.com",
+      messagingSenderId: "364492185043",
+      appId: "1:364492185043:web:6cab76272a770512"
+    };
 
-function getDoctorResults() {
-  var x = document.getElementById("specialty_uid").value;
-  //paramters from text array is the following: name and specialty_uid
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    const database = firebase.database();
 
-  /*BetterDoctor, Inc. API*/
-  var api_key = ''; / INSERT API HERE
+    $('#read-button').click(() => {
+      console.log('function clicked');
 
-  var resource_url = "https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=" + specialty
-  + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=" + api_key;
+      $(document.getElementById('content-placeholder')).empty();
 
-  $.get(resource_url, function (data) {
-      // data: { meta: {<metadata>}, data: {<array[Practice]>} }
-      var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
-      document.getElementById('content-placeholder').innerHTML = template(data);
+      database.ref('betterdoc').once('value', (snapshot) => {
+        const data = snapshot.val();
+        const specialty_uid = document.getElementById("specialty_uid").value;
+
+        var resource_url = "https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=" + specialty_uid
+        + "&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=7&user_key=" + data;
+
+        $.get(resource_url, function (data) {
+            // data: { meta: {<metadata>}, data: {<array[Practice]>} }
+            var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
+            document.getElementById('content-placeholder').innerHTML = template(data);
+        });
+      });
+    });
   });
-}
