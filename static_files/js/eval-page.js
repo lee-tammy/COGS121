e94f047f59afa5f205df14a71a13a8642fb88456
Tammy Lee <tal066@ucsd.edu>
2019-06-04 21:03:36 -0700

    shows filler text only when there is no chart in eval-page

--- static_files/js/eval-page.js
+++ static_files/js/eval-page.js
@@ -16,8 +16,6 @@ if (!firebase.apps.length) {
 const database = firebase.database();
 const user = localStorage.getItem("user");
 $(document).ready(()=>{
-    
-
     // gets the username of the client profile you're viewing
     let path = window.location.href;
     let segments = path.split('#');
@@ -46,86 +44,94 @@ $(document).ready(()=>{
 function drawChart(eval, username) {
   database.ref('users/' + username + '/evals/' + eval).once('value', (snapshot)=>{
     const data = snapshot.val();
-
-    const arrayToTable = [];
     
-    let questions = ['q1', 'q2', 'q3', 'q4', 'q5']
-    const firstRow = [];
-    let title = ''
-    let numOfQuestions = 0
-    if(eval == 'sleep'){
-      title = 'Sleep Evaluation'
-      numOfQuestions = 3;
-      questions[0] = 'I went to sleep at a good time...'
-      questions[1] = 'I slept well...'
-      questions[2] = 'I was sleepy during the day...'
-    }else if(eval == 'friends'){
-      numOfQuestions = 5;
-      title = 'Friends Evaluation'
-      questions[0] = 'I have friends I can talk to...'
-      questions[1] = 'I have many friends...'
-      questions[2] = 'I am happy with my friends...'
-      questions[3] = 'I am fighting with my friends...'
-      questions[4] = 'I chatted with friends after school...'
-    }else if(eval == 'family'){
-      title = 'Family Evaluation'
-      numOfQuestions = 4;
-      questions[0] = 'I am happy with my family...'
-      questions[1] = 'I like talking with my family...'
-      questions[2] = 'I have a supportive family...'
-      questions[3] = 'I have a loving family...'
-    }else if(eval == 'activities'){
-      title = 'Activities Evaluation'
-      numOfQuestions = 5;
-      questions[0] = 'I played outside...'
-      questions[1] = 'I like to read books...'
-      questions[2] = 'I watched TV...'
-      questions[3] = 'I played video games...'
-      questions[4] = 'I spent time watching videos on my phone...'
-    }else if(eval == 'attention'){
-      title = 'Attention Evaluation'
-      numOfQuestions = 5;
-      questions[0] = 'I had a hard time paying attention...'
-      questions[1] = 'I daydream a lot...'
-      questions[2] = 'I like to do other activities at the same time...'
-      questions[3] = 'I sometimes stop what I\'m doing to do other things...'
-      questions[4] = 'I am a good listener...'
-    }
-    firstRow.push('')
-    for(let i = 0; i < numOfQuestions; i++){
-      firstRow.push(questions[i])
-    }
-    arrayToTable.push(firstRow);
-
-    for(let i in Object.keys(data)){
-      let row = [];
-      const dat = '' + Object.keys(data)[i]
-      row.push(dat)
-
-      database.ref('users/' + username + '/evals/' + eval + '/' + dat).once('value',(snapshot)=>{
-
-        for(const i in snapshot.val()){
-          row.push(parseInt(snapshot.val()[i]))
-        }
-        
-        arrayToTable.push(row)
-        row = []
-
-        var d = google.visualization.arrayToDataTable(arrayToTable);
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-        
-        var options = {
-          title: title,
-          curveType: 'function',
-          legend: { position: 'bottom' },
-          height: screen.height / 2 + 200,
-          width: screen.width,
-          vAxis: { gridlines: { count: 4 } }
-        };
-        
-          chart.draw(d, options);
-      });
+    if(data != null){
+      const arrayToTable = [];
       
+      let questions = ['q1', 'q2', 'q3', 'q4', 'q5']
+      const firstRow = [];
+      let title = ''
+      let numOfQuestions = 0
+      if(eval == 'sleep'){
+        title = 'Sleep Evaluation'
+        numOfQuestions = 3;
+        questions[0] = 'I went to sleep at a good time...'
+        questions[1] = 'I slept well...'
+        questions[2] = 'I was sleepy during the day...'
+      }else if(eval == 'friends'){
+        numOfQuestions = 5;
+        title = 'Friends Evaluation'
+        questions[0] = 'I have friends I can talk to...'
+        questions[1] = 'I have many friends...'
+        questions[2] = 'I am happy with my friends...'
+        questions[3] = 'I am fighting with my friends...'
+        questions[4] = 'I chatted with friends after school...'
+      }else if(eval == 'family'){
+        title = 'Family Evaluation'
+        numOfQuestions = 4;
+        questions[0] = 'I am happy with my family...'
+        questions[1] = 'I like talking with my family...'
+        questions[2] = 'I have a supportive family...'
+        questions[3] = 'I have a loving family...'
+      }else if(eval == 'activities'){
+        title = 'Activities Evaluation'
+        numOfQuestions = 5;
+        questions[0] = 'I played outside...'
+        questions[1] = 'I like to read books...'
+        questions[2] = 'I watched TV...'
+        questions[3] = 'I played video games...'
+        questions[4] = 'I spent time watching videos on my phone...'
+      }else if(eval == 'attention'){
+        title = 'Attention Evaluation'
+        numOfQuestions = 5;
+        questions[0] = 'I had a hard time paying attention...'
+        questions[1] = 'I daydream a lot...'
+        questions[2] = 'I like to do other activities at the same time...'
+        questions[3] = 'I sometimes stop what I\'m doing to do other things...'
+        questions[4] = 'I am a good listener...'
+      }
+      firstRow.push('')
+      for(let i = 0; i < numOfQuestions; i++){
+        firstRow.push(questions[i])
+      }
+      arrayToTable.push(firstRow);
+
+      for(let i in Object.keys(data)){
+        let row = [];
+        const dat = '' + Object.keys(data)[i]
+        row.push(dat)
+
+        database.ref('users/' + username + '/evals/' + eval + '/' + dat).once('value',(snapshot)=>{
+
+          for(const i in snapshot.val()){
+            row.push(parseInt(snapshot.val()[i]))
+          }
+          
+          arrayToTable.push(row)
+          row = []
+
+          var d = google.visualization.arrayToDataTable(arrayToTable);
+          var chart = new google.visualization.LineChart(document.getElementById('curve-chart'));
+          
+          var options = {
+            title: title,
+            curveType: 'function',
+            legend: { position: 'bottom' },
+            height: screen.height / 2 + 200,
+            width: screen.width,
+            vAxis: { gridlines: { count: 4 } }
+          };
+          
+            chart.draw(d, options);
+        });
+        document.getElementById("legend").style.display = "block"
+        document.getElementById("curve-chart").style.display = "block"
+        document.getElementById("no-chart").style.display = "none"
+      }
+    }else{
+      document.getElementById("legend").style.display = "none"
+      document.getElementById("curve-chart").style.display = "none"
+      document.getElementById("no-chart").style.display = "block"
     }
     
     
